ff1ff56f748d514bb492b0f4220801c1009f5203
Florian Kretlow <florian.kretlow@innoq.com>
2023-12-05 19:21:18 +0100

    Day 5

---
+++ src/aoc_23/05.clj
@@ -0,0 +1,87 @@
+(ns aoc-23.05
+  (:require
+   [aoc-23.util :refer [get-puzzle-input parse-longs]]
+   [clojure.string :as str])
+  (:import
+   [java.lang Long]))
+
+
+(defn- ->map-fn [map-seq]
+  (fn [n]
+    (loop [ranges (partition 3 map-seq)]
+      (if-let [[dest-start src-start len] (first ranges)]
+        (if (<= src-start n (dec (+ src-start len)))
+          (+ n (- dest-start src-start))
+          (recur (rest ranges)))
+        n))))
+
+
+(defn part-1 [seed-seq map-seqs]
+  (let [map-chain-fn (apply comp (reverse (map ->map-fn map-seqs)))]
+    (as-> seed-seq seeds (apply min (map map-chain-fn seeds)))))
+
+
+(defn- intersect-ranges [r1 r2]
+  (let [[r1 r2] (sort [r1 r2])]
+    [(max (r1 0) (r2 0)) (min (r1 1) (r2 1))]))
+
+
+(defn- shift-range [d r]
+  [(+ d (r 0)) (+ d (r 1))])
+
+
+(defn- insert-zero-shift-ranges [mapping]
+  (reverse
+   (let [mapping'
+         (reduce
+          (fn [mapping [r' d']]
+            (cond-> mapping
+              (< (get (ffirst mapping) 1 0) (r' 0)) (conj [[(get (ffirst mapping) 1 0) (r' 0)] 0])
+              :always (conj [r' d'])))
+          nil
+          mapping)]
+     (conj mapping' [[(get (ffirst mapping') 1 0) Long/MAX_VALUE] 0]))))
+
+
+(defn- ->mapping [map-seq]
+  (->> (for [[dest-start src-start len] (partition 3 map-seq)]
+         [[src-start (+ src-start len)] (- dest-start src-start)])
+       (sort-by first)
+       (insert-zero-shift-ranges)))
+
+
+(defn- apply-mapping-to-range [mapping r]
+  (->> mapping
+       (drop-while (fn [[r_m]] (<= (r_m 1) (r 0))))
+       (take-while (fn [[r_m]] (< (r_m 0) (r 1))))
+       (map (fn [[r_m d_m]] [(intersect-ranges r r_m) d_m]))))
+
+
+(defn- apply-mappings-to-shifted-range [mappings [r d]]
+  (if (seq mappings)
+    (mapcat
+     (partial apply-mappings-to-shifted-range (rest mappings))
+     (->> (shift-range d r)
+          (apply-mapping-to-range (first mappings))
+          (map (fn [[r' d']] [(shift-range (- d) r') (+ d' d)]))))
+    [[r d]]))
+
+
+(defn- ->seed-ranges [seed-seq]
+  (->> (partition 2 seed-seq)
+       (map (fn [[start len]] [start (+ start len)]))))
+
+
+(defn part-2 [seed-seq map-seqs]
+  (let [seed-ranges (->seed-ranges seed-seq)
+        mappings (map ->mapping map-seqs)]
+    (->> (mapcat (partial apply-mappings-to-shifted-range mappings)
+                 (map (fn [r] [r 0]) seed-ranges))
+         (map (fn [[r d]] (+ (r 0) d)))
+         (apply min))))
+
+
+(let [input (get-puzzle-input 5)
+      [seed-seq & map-seqs] (map parse-longs (str/split input #"\n\n"))]
+  (println "Part 1: " (part-1 seed-seq map-seqs))
+  (println "Part 2: " (part-2 seed-seq map-seqs)))
--- src/aoc_23/util.clj
+++ src/aoc_23/util.clj
@@ -43,3 +43,25 @@
 
 (defn exp [b e]
   (apply * (repeat e b)))
+
+
+(defn parse-longs 
+  "Return a lazy seq of all the integers in the string `s`, e.g. `\"a 12 b 3.4\"` -> `(12 3 4)`"
+  [s] (map parse-long (re-seq #"\d+" s)))
+
+
+(defn merge-ranges 
+  "Given any number of half open integer intervals of the form `[start end]`
+  (end non-inclusive), reduce them to a set of non-overlapping ranges.
+  E.g. `[1 3] [5 7] [2 4]` -> `([1 4] [5 7])`"
+  [& rs]
+  (letfn [(-overlap? [r1 r2] (let [[r1 r2] (sort [r1 r2])] (> (r1 1) (r2 0))))
+          (-merge [r1 r2] [(min (r1 0) (r2 0)) (max (r1 1) (r2 1))])]
+    (sort
+     (reduce
+      (fn [acc r]
+        (if (and (seq acc) (-overlap? r (first acc)))
+          (conj (rest acc) (-merge r (first acc)))
+          (conj acc r)))
+      nil
+      (sort (map vec rs))))))
