67dd5147e054795c5dad38e4db2274ca85fd318f
Florian Kretlow <florian.kretlow@innoq.com>
2023-12-16 10:05:06 +0100

    Day 10

---
+++ src/aoc/23/10.clj
@@ -0,0 +1,110 @@
+(ns aoc.23.10
+  (:require
+   [aoc.lib.io :refer [get-puzzle-input]]
+   [aoc.lib.matrix :refer [char-matrix mfind mget mshape v*scalar v+ v-]]
+   [aoc.lib.seq :refer [find-first]]
+   [clojure.string :as str]))
+
+
+(def ^:private north [-1 0])
+(def ^:private west [0 -1])
+(def ^:private south [1 0])
+(def ^:private east [0 1])
+(def ^:private shape-connections {\| #{north south}
+                                  \- #{west east}
+                                  \L #{north east}
+                                  \J #{north west}
+                                  \7 #{south west}
+                                  \F #{south east}
+                                  \. #{}
+                                  \S #{north west south east}})
+
+
+(defn- inside? [[h w] [i j]] (and (<= 0 i (dec h)) (<= 0 j (dec w))))
+
+
+(defn- connect?
+  "Does the pipe shape at p1 on the grid connect to p2?"
+  [grid p1 p2]
+  (let [d (v- p2 p1)]
+    (boolean (and (inside? (mshape grid) p2)
+                  (#{north west south east} d)
+                  ((get shape-connections (mget grid p1)) d)
+                  ((get shape-connections (mget grid p2)) (v*scalar d -1))))))
+
+
+(defn- connected-tiles
+  "Return all tiles that are connected to the tile at p on the grid."
+  [grid p]
+  (for [d (shape-connections (mget grid p))
+        :let [p' (v+ p d)]
+        :when (connect? grid p p')]
+    p'))
+
+
+(defn- next-tile
+  "Given the path as a 'stack' of last visited positions, return the next position."
+  [grid [cur prev]]
+  (find-first #(not= prev %) (connected-tiles grid cur)))
+
+
+(defn- find-loop
+  "Find the one loop on the grid that starts and returns at the tile labeled S."
+  [grid]
+  (let [start (first (mfind grid #(= \S %)))
+        cur (first (connected-tiles grid start))]
+    (loop [[cur, :as path] (list cur start)]
+      (if (= cur start)
+        path
+        (recur (cons (next-tile grid path) path))))))
+
+
+(defn part-1 [grid] (long (/ (count (find-loop grid)) 2)))
+
+
+(defn- straight? [grid pos]
+  (let [shape (mget grid pos)]
+    (or (#{\| \-} shape)
+        ;; Ugly but necessary because S can be any shape:
+        (and (= \S shape)
+             (or (and (connect? grid pos (v+ pos north)) (connect? grid pos (v+ pos south)))
+                 (and (connect? grid pos (v+ pos west)) (connect? grid pos (v+ pos east))))))))
+
+
+(defn- analyze-loop [grid path]
+  (loop [path path
+         corners (transient #{})
+         straight-segments (transient #{})]
+    (if-let [p (first path)]
+      (if (straight? grid p)
+        (recur (rest path) corners (conj! straight-segments p))
+        (recur (rest path) (conj! corners p) straight-segments))
+      {:corners (count corners)
+       :straight-segments (count straight-segments)})))
+
+
+(defn- shoelace
+  "https://en.wikipedia.org/wiki/Shoelace_formula"
+  [path]
+  (abs
+   (reduce
+    (fn [acc [[i1 j1] [i2 j2]]]
+      (+ acc (/ (* (+ j1 j2) (- i1 i2)) 2.0)))
+    0
+    (partition 2 1 path))))
+
+
+(defn- part-2
+  "The loop is an irregular polygon. Compute the area and subtract fractions
+  of tiles to accomodate for the fact that a pipe occupies one 'unit' of area."
+  [grid]
+  (let [path (find-loop grid)
+        {:keys [corners straight-segments]} (analyze-loop grid path)]
+    (- (shoelace path)
+       (/ (- corners 2) 2)
+       (/ straight-segments 2))))
+
+
+(let [grid (char-matrix (str/split-lines (get-puzzle-input 23 10)))]
+  (println "Part 1: " (time (part-1 grid)))
+  (println "Part 2: " (time (part-2 grid))))
--- src/aoc/lib/matrix.clj
+++ src/aoc/lib/matrix.clj
@@ -28,3 +28,28 @@
        (map-indexed (fn [i js] (when (seq js) (for [j js] [i j]))))
        (filter some?)
        (apply concat)))
+
+
+(defn char-matrix [lines]
+  (assert (apply = (map count lines)) "different line lengths")
+  (into-array (map char-array lines)))
+
+
+(defn mget [m [i j]] (aget m i j))
+(defn mset [m [i j] x] (aset m i j x))
+
+
+(defn mshape [m] [(count m) (count (first m))])
+
+
+(defn mfind [m pred]
+  (let [[h w] (mshape m)]
+    (for [i (range h), j (range w)
+          :when (pred (mget m [i j]))]
+      [i j])))
+
+
+(defn v+ [& vs] (mapv (partial apply +) (apply (partial map vector) vs)))
+(defn v- [& vs] (mapv (partial apply -) (apply (partial map vector) vs)))
+(defn v*scalar [v x] (mapv #(* x %) v))
+
