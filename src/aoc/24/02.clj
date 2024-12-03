b798ca72b6f19b3ed0bb893321dacd4e5645d324
Florian Kretlow <florian.kretlow@innoq.com>
2024-12-03 09:34:45 +0100

    24/02-2

--- src/aoc/24/02.clj
+++ src/aoc/24/02.clj
@@ -2,40 +2,39 @@
   (:require
    [aoc.lib.io :refer [get-puzzle-input]]
    [aoc.lib.parsing :refer [parse-longs]]
+   [aoc.lib.seq :refer [drop-nth]]
    [clojure.string :as str]))
-
-
-(def ^:private test-input
-  "7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9")
 
 
 (defn- parse-lists [input]
   (map parse-longs (str/split-lines input)))
 
 
-(defn- is-safe-step? [dir? [a b]]
-  (let [d (- b a)] (and (dir? d) (< 0 (abs d) 4))))
+(defn is-safe-level? [levels i]
+  (let [start (max 0 (dec i))
+        end (+ i 2)
+        window (take (- end start) (drop start levels))
+        steps (partition 2 1 window)]
+    (and (->> steps
+              (map (partial apply #(abs (- %2 %1))))
+              (every? #(< 0 % 4)))
+         (or (apply < window) (apply > window)))))
 
 
-(defn- is-safe? [[fst snd, :as report]]
-  (let [dir? (cond (< fst snd) pos?, (> fst snd) neg?, :else zero?)]
-    (->> (partition 2 1 report)
-         (map (partial is-safe-step? dir?))
-         (every? true?))))
+(defn is-safe-report? [can-drop? levels]
+  (or (every? #(is-safe-level? levels %) (range (count levels)))
+      (and can-drop?
+           (some #(is-safe-report? false (drop-nth % levels)) (range (count levels))))))
 
 
 (defn part-1 [reports]
-  (count (filter is-safe? reports)))
+  (count (filter #(is-safe-report? false %) reports)))
 
 
-(comment (let [reports (parse-lists test-input)]
-           (map is-safe? reports)))
+(defn part-2 [reports]
+  (count (filter #(is-safe-report? true %) reports)))
 
 
 (let [reports (parse-lists (get-puzzle-input 24 2))]
-  (println "Part 1:" (part-1 reports)))
+  (println "Part 1:" (part-1 reports))
+  (println "Part 2:" (part-2 reports)))
