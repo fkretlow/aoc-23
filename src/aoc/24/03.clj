86041100e242ec1d8846400fdb84ced82527e2a0
Florian Kretlow <florian.kretlow@innoq.com>
2024-12-03 19:05:57 +0100

    24/03 as fsm

--- src/aoc/24/03.clj
+++ src/aoc/24/03.clj
@@ -4,35 +4,45 @@
    [aoc.lib.parsing :refer [parse-longs]]
    [clojure.string :as str]))
 
+; We define a little state machine.
+; The state is a map containing the current sum as accumulation value, and, for part 2, whether we should
+; skip mul instructions, e.g. `{:acc 0, :skip? false}`
+; Events are vectors containing their type followed by possible payloads, e.g. `[:do]`, `[:don't]`, or `[:mul 1 2]`
+; The multimethod `process` accepts the state and one event and returns the next state.
+; Parsing is very easy with regular expressions.
+
+(defmulti process (fn [_state [type, :as _event]] type))
+(defmethod process :mul [state [_ x y]] (if (:skip? state) state (update state :acc #(+ % (* x y)))))
+(defmethod process :do [state _] (assoc state :skip? false))
+(defmethod process :don't [state _] (assoc state :skip? true))
+
+(defn parse-events [{:keys [skips?], :as _options} input]
+  (let [pattern (if skips?
+                  #"(?:mul\(\d{1,3},\d{1,3}\))|(?:do\(\))|(?:don't\(\))"
+                  #"mul\(\d{1,3},\d{1,3}\)")]
+    (for [match (re-seq pattern input)]
+      (cond
+        (str/starts-with? match "mul")
+        (vec (conj (parse-longs match) :mul))
+
+        (str/starts-with? match "don't")
+        [:don't]
+
+        (str/starts-with? match "do")
+        [:do]
+
+        :else
+        (throw (ex-info "funny match" {:match match}))))))
 
 (defn part-1 [input]
-  (let [pattern #"mul\(\d{1,3},\d{1,3}\)"]
-    (->> (re-seq pattern input)
-         (map parse-longs)
-         (map #(apply * %))
-         (reduce +))))
-
+  (let [events (parse-events {:skips? false} input)]
+    (-> (reduce process {:acc 0} events)
+        :acc)))
 
 (defn part-2 [input]
-  (let [pattern #"(?:mul\(\d{1,3},\d{1,3}\))|(?:do\(\))|(?:don't\(\))"
-        instructions (re-seq pattern input)]
-    (loop [acc 0
-           skip? false
-           [instr & more] instructions]
-      (cond
-        (not instr)
-        acc
-
-        skip?
-        (recur acc (not= instr "do()") more)
-
-        (str/starts-with? instr "mul")
-        (let [prod (->> (parse-longs instr) (apply *))]
-          (recur (+ acc prod) false more))
-        
-        :else
-        (recur acc (= instr "don't()") more)))))
-
+  (let [events (parse-events {:skips? true} input)]
+    (-> (reduce process {:acc 0, :skip? false} events)
+        :acc)))
 
 (let [input (get-puzzle-input 24 3)]
   (println "Part 1:" (part-1 input))
